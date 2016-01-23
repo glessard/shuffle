a6ab402975aaae8970b19b77e75eea6cce0e2809
Guillaume Lessard <dhtnstff@gmail.com>
2016-01-23 16:35:13 -0700

    A new ShuffledSequence type to replace PermutationGenerator

--- Sources/shuffle.swift
+++ Sources/shuffle.swift
@@ -11,19 +11,78 @@
 #if os(Linux)
 import Glibc
 #else
-import Darwin.C
+import func Darwin.C.stdlib.arc4random_uniform
 #endif
+
+/// Get a sequence/generator that will return a collection's elements in a random order.
+/// The input collection is not modified.
+///
+/// - parameter c: The collection to be shuffled
+/// - returns: A sequence of of `c`'s elements, lazily shuffled.
+
+public func shuffle<C: CollectionType>(c: C) -> ShuffledSequence<C>
+{
+  return ShuffledSequence(c)
+}
 
 public extension CollectionType
 {
   /// Get a sequence/generator that will return a collection's elements in a random order.
   /// The input collection is not modified.
   ///
-  /// - returns: A `PermutationGenerator` of `c`'s elements, shuffled.
+  /// - returns: A sequence of of `self`'s elements, lazily shuffled.
 
-  func shuffle() -> PermutationGenerator<Self, IndexShuffler<Self.Index>>
+  public func shuffled() -> ShuffledSequence<Self>
   {
-    return PermutationGenerator(elements: self, indices: IndexShuffler(self.indices))
+    return ShuffledSequence(self)
+  }
+}
+
+
+/// A stepwise implementation of the Knuth Shuffle (a.k.a. Fisher-Yates Shuffle).
+/// The input collection is not modified: the shuffling itself is done
+/// using an adjunct array of indices.
+
+public struct ShuffledSequence<C: CollectionType>: SequenceType, GeneratorType
+{
+  public let collection: C
+  public let count: Int
+
+  public private(set) var step = -1
+  private var i: [C.Index]
+
+  public init(_ input: C)
+  {
+    collection = input
+    i = Array(input.indices)
+    count = i.count
+  }
+
+  public mutating func next() -> C.Generator.Element?
+  {
+    // current position in the array
+    step += 1
+
+    if step < count
+    {
+      // select a random Index from the rest of the array
+#if os(Linux)
+      let j = step + Int(random() % (count-step))
+#else
+      let j = step + Int(arc4random_uniform(UInt32(count-step)))
+#endif
+
+      // swap that Index with the Index present at the current step in the array
+      if j != step // swap 2beta6 calls `fatalError` if the two items are identical.
+      {
+        swap(&i[j], &i[step])
+      }
+
+      // return the new random Index.
+      return collection[i[step]]
+    }
+
+    return nil
   }
 }
 
