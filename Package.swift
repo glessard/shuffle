d0157f0f05c1eaaae73905bd855c36cc7fa40875
Guillaume Lessard <dhtnstff@gmail.com>
2017-08-07 23:24:51 -0600

    update package description to swift 4 format

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:4.0
+
 import PackageDescription
+
+#if swift(>=4.0)
+
+let package = Package(
+  name: "Shuffle",
+  products: [ .library(name: "Shuffle", type: .static, targets: ["Shuffle"]) ],
+  targets: [
+    .target(name: "Shuffle", path: "Sources"),
+    .testTarget(name: "ShuffleTests", dependencies: ["Shuffle"])
+  ],
+  swiftLanguageVersions: [3,4]
+)
+
+#else
 
 let package = Package(
 	name: "Shuffle"
 )
+
+#endif
